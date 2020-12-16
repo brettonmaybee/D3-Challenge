5cd7ca702c224053aa11a0ac336dd0799be67a7a
brettonmaybee <brettonmaybee@users.noreply.github.com>
2020-12-15 23:31:57 -0800

    commit

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,11 +1,11 @@
-var svgWidth = 1024;
-var svgHeight = 768;
+var svgWidth = 900;
+var svgHeight = 600;
 
 var margin = {
-  top: 50,
-  right: 40,
+  top: 0,
+  right: 0,
   bottom: 100,
-  left: 100
+  left: 80
 };
 
 var width = svgWidth - margin.left - margin.right;
@@ -25,7 +25,8 @@ d3.csv("../assets/data/data.csv").then(function(data) {
     data.forEach(function(data) {
         data.healthcare= +data.healthcare;
         data.poverty= +data.poverty;
-    console.log(data)
+    console.log(data);
+    console.log("Abbreviation:",data.abbr);
     console.log("State:", data.state);
     console.log("Poverty:", data.poverty);
     console.log("Healthcare:", data.healthcare);
@@ -52,62 +53,60 @@ d3.csv("../assets/data/data.csv").then(function(data) {
     chartGroup.append("g")
       .call(leftAxis);
 
-    // Create Circles
-    var circlesGroup = chartGroup.selectAll("circle")
-    .data(data)
-    .enter()
-    .append("circle")
-    .attr("cx", d => xLinearScale(d.poverty))
-    .attr("cy", d => yLinearScale(d.healthcare))
-    .attr("r", "10")
-    .attr("fill", 'blue')
-    .attr("opacity", ".5")
-    
-    // Create Abbreviations
-    var textGroup=chartGroup.selectAll("text")
-    .data(data)
-    .enter()
-    .append("text")
-    .attr("x", d=>xLinearScale(d.poverty))
-    .attr("y", d=>yLinearScale(d.healthcare))
-    //.offset([-60, 80])
-    .text(function(data) {
-      return (`${data.abbr}`);
-    });
-
+      // Create Circles
+    var circlesGroup=chartGroup.selectAll("circle")
+      .data(data)
+      .enter()
+      .append("circle")
+      .attr("cx", d => xLinearScale(d.poverty))
+      .attr("cy", d => yLinearScale(d.healthcare))
+      .attr("r", "15")
+      .attr("fill", 'blue')
+      .attr("opacity", ".3")
+           
     // Initialize tool tip
-    var toolTip = d3.tip()
-      .attr("class", "tooltip")
-      .offset([80, -60])
-      .html(function(data) {
-        return (`${data.state}<br>Poverty: ${data.poverty}<br>healthcare: ${data.healthcare}`);
-      });
+    //var toolTip = d3.tip()
+    //.attr("class", "tooltip")
+    //.offset([80, -60])
+    //.html(function(data) {
+    //  return (`${data.state}<br>Poverty: ${data.poverty}<br>healthcare: ${data.healthcare}`);
+    //});
 
     // Create tooltip in the chart
-    chartGroup.call(toolTip);
+    //chartGroup.call(toolTip);
 
     // Create event listeners to display and hide the tooltip
-    circlesGroup.on("click", function(data) {
-      toolTip.show(data, this);
-    })
+     // circlesGroup.on("click", function(data) {
+     // toolTip.show(data, this);
+    //})
       // onmouseout event
-      .on("mouseout", function(data, index) {
-        toolTip.hide(data);
-      });
+      //.on("mouseout", function(data, index) {
+      //  toolTip.hide(data);
+      //});
 
     // Create axes labels
     chartGroup.append("text")
       .attr("transform", "rotate(-90)")
       .attr("y", 0 - margin.left + 40)
       .attr("x", 0 - (height / 2))
-      .attr("dy", "1em")
       .attr("class", "axisText")
-      .text("Healthcare");
+      .text("Lacks Healthcare (%)");
 
     chartGroup.append("text")
       .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
       .attr("class", "axisText")
-      .text("Poverty");
+      .text("In Poverty (%)");
+
+      // Create Abbreviations
+    var textGroup=chartGroup.selectAll("text")
+      .data(data)
+      .enter()
+      .append("text")
+      .attr("x", d=>xLinearScale(d.poverty)-12)
+      .attr("y", d=>yLinearScale(d.healthcare)+7)
+      .text(function(data) {
+        return (`${data.abbr}`);
+      });     
 
   }).catch(function(error) {
     console.log(error);
